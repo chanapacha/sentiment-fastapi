68d27651bd3394626161ba6ad95857284e970c23
Chanapa Chareesan <chanapa.cartoon@gmail.com>
2024-04-27 01:39:36 +0700

    edit code

--- st_app.py
+++ st_app.py
@@ -1,18 +1,16 @@
-
-
 import streamlit as st
 import pandas as pd
 from SentimentAnalyzer import *
 from utils import split_fn
 
-
 # Initialize SentimentAnalyzer
-analyzer = SentimentAnalyzer()
+if 'analyzer' not in st.session_state:
+    st.session_state.analyzer = SentimentAnalyzer()
 
 # Function to analyze sentiment of text
 @st.cache_data
 def analyze_sentiment_text(text):
-    sentiment_pred, sentiment_conf, trigger = analyzer.sentiment_classify(text)
+    sentiment_pred, sentiment_conf, trigger = st.session_state.analyzer.sentiment_classify(text)
     return sentiment_pred, sentiment_conf, trigger
 
 # Function to analyze sentiment of uploaded Excel file
@@ -21,7 +19,7 @@ def analyze_sentiment_excel(file):
     df = pd.read_excel(file)
     results = []
     for text in df['text']:
-        sentiment_pred, sentiment_conf, trigger = analyzer.sentiment_classify(text)
+        sentiment_pred, sentiment_conf, trigger = st.session_state.analyzer.sentiment_classify(text)
         results.append({"sentiment": sentiment_pred, "confidence": sentiment_conf, "trigger": trigger})
     df_result = pd.concat([df, pd.DataFrame(results)], axis=1)
     return df_result
@@ -43,6 +41,11 @@ def main():
 
     # File Upload Section
     st.subheader('File Upload')
+    st.write('Please ensure that the uploaded Excel file contains a column named :blue[text]')
+    st.write('For example:')
+    st.write(pd.DataFrame({
+    'text': ['แอปตัวนี้ทำไมมันใช้งานยากจัง', 'ที่นี่ดูแลดีใส่ใจลูกค้าดีมากเลยครั้งหน้าจะมาใช้บริการอีกแน่นอน', 'ต้องการร้องเรียนพฤติกรรมของพนักงาน']
+}))
     uploaded_file = st.file_uploader('Upload Excel file', type=['xlsx', 'xls'])
     if uploaded_file is not None:
         if st.button('Analyze Excel'):
@@ -50,6 +53,6 @@ def main():
             st.write('Analysis Results:')
             st.write(df_result)
 
-
 if __name__ == '__main__':
     main()
+
